f2e29c74f2669ed15d88693af3d863a41c0a7bc8
Egor Kosolapov <greezzly7@gmail.com>
2014-01-15 12:52:22 +0400

    socials2

--- application/views/main/likebox.php
+++ application/views/main/likebox.php
@@ -9,7 +9,7 @@
 					</script>
 			</li>
 			<li>
-				<iframe src="//www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2FFacebookDevelopers&amp;width=470&amp;height=300&amp;colorscheme=light&amp;show_faces=true&amp;header=false&amp;stream=false&amp;show_border=true&amp;appId=384403321677819" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:470px; height:300px;" allowTransparency="true"></iframe>
+				<iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2F%25D0%259C%25D0%25B0%25D0%25B3%25D0%25B0%25D0%25B7%25D0%25B8%25D0%25BD-%25D0%25B4%25D0%25BB%25D1%258F-%25D0%25BD%25D0%25B0%25D1%2588%25D0%25B8%25D1%2585-%25D0%2594%25D0%25B5%25D1%2582%25D0%25BE%25D0%25BA%2F442195032573727&amp;width=470&amp;height=300&amp;colorscheme=light&amp;show_faces=true&amp;header=false&amp;stream=false&amp;show_border=true&amp;appId=622720151134475" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:470px; height:300px;" allowTransparency="true"></iframe>
 			</li>
 			<li class="helper"></li>
 		</ul>
