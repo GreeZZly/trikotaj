f5f5a749c84736155ca7e1676cd79cf28cdacb59
Egor Kosolapov <greezzly7@gmail.com>
2014-01-15 13:44:17 +0400

    commercial_bg

--- application/views/main/commercial.php
+++ application/views/main/commercial.php
@@ -1,7 +1,9 @@
+<div id="pusto_mesto"></div>
 <div id="commercial_background">
 	<div class="center_block" id="commercial">
 		<div>
-			<img src="/include/images/commercial.png">
+			<img src="/include/images/hero-kids-winter4.png" id="commercial_img">
 		</div>
 	</div>
 </div>
+<div id="pusto_mesto_cvetno"></div>
