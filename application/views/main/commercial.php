741d41d62638e55de3a61417c7134660a7a24af5
Egor Kosolapov <greezzly7@gmail.com>
2014-03-04 14:32:35 +0400

    drupal yoba!!

--- application/views/main/commercial.php
+++
@@ -1,9 +0,0 @@
-<div id="pusto_mesto"></div>
-<div id="commercial_background">
-	<div class="center_block" id="commercial">
-		<div>
-			<img src="/include/images/hero-kids-winter4.png" id="commercial_img">
-		</div>
-	</div>
-</div>
-<div id="pusto_mesto_cvetno"></div>
--- home.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include("applications/views/main/htmlheader.html");
-	include("applications/views/main/header.php");
-	include("applications/views/main/navbar.php");
-	include("applications/views/main/htmlfooter.html");
-
-?>
